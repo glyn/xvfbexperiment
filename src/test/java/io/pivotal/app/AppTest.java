ad74c4ca11bb76664681e6730433875992fd017d
Glyn Normington <gnormington@pivotal.io>
2016-05-11 16:03:24 +0000

    Reproduce port problem

--- src/test/java/io/pivotal/app/AppTest.java
+++ src/test/java/io/pivotal/app/AppTest.java
@@ -1,10 +1,12 @@
 package io.pivotal.app;
 
+import java.io.File;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
 
 import static junit.framework.Assert.assertTrue;
 
@@ -13,7 +15,15 @@ public class AppTest {
 
     @Before
     public void setUp() {
-        driver = new FirefoxDriver();
+        // Setup firefox binary to start in Xvfb        
+        String Xport = System.getProperty(
+                "lmportal.xvfb.id", ":1");
+        final File firefoxPath = new File(System.getProperty(
+                "lmportal.deploy.firefox.path", "/usr/bin/firefox"));
+        FirefoxBinary firefoxBinary = new FirefoxBinary(firefoxPath);
+        firefoxBinary.setEnvironmentProperty("DISPLAY", Xport);
+
+        driver = new FirefoxDriver(firefoxBinary, null);
     }
 
     @After
