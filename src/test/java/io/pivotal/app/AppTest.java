ca28cd01732cee39ba9bbaf7be1cae95c41ffee7
Glyn Normington <gnormington@pivotal.io>
2016-05-11 14:42:08 +0000

    Replace with selenium test

--- src/test/java/io/pivotal/app/AppTest.java
+++ src/test/java/io/pivotal/app/AppTest.java
@@ -1,38 +1,36 @@
 package io.pivotal.app;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+import static junit.framework.Assert.assertTrue;
+
+public class SearchTest {
+    private WebDriver driver;
+
+    @Before
+    public void setUp() {
+        driver = new FirefoxDriver();
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @After
+    public void tearDown() {
+        driver.close();
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void verifyThatStockholmCanBeFound() {
+        HomePage home = new HomePage(driver);
+        SearchResultPage searchResult = home.searchFor("Stockholm");
+
+        LocationPage stockholm = searchResult.clickOnTopSearchResultLink();
+
+        String expected = "Stockholm";
+        String actual = stockholm.getHeadLine();
+        assertTrue(actual.contains(expected));
     }
 }
+
